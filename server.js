77057b1305750396d3034c0ee7bcd09fc3fe01d0
Kunthy.Sen <kunthy.sen@PNCLB32-09.pnc.lan>
2021-05-20 09:04:36 +0700

    Update

--- server.js
+++ server.js
@@ -7,4 +7,20 @@ app.use(express.json());
 app.use(express.urlencoded());
 app.use(express.static('public'));
 
-app.get('/',(req,res) =>res.send('Hello project'));
+app.get('/text',(req,res) =>res.send('Hello project'));
+let Message = {
+    text: "hello world",
+}
+
+
+app.post('/text', (req, res)=>{
+    let text = req.body;
+    Message = text ;
+    res.send(text)
+    
+})
+
+app.get('/text', (req, res)=>{
+    res.send(Message)
+
+})
