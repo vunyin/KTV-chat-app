03c91af98123e5f5049f1f55e1f246f571fe5f08
Kunthy.Sen <kunthy.sen@PNCLB32-09.pnc.lan>
2021-05-24 14:20:57 +0700

    NEW

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const app = express();
+const fs = require('fs');
+let dataUsers =(JSON.parse(fs.readFileSync('dataUsers.json')));
 
 app.listen(process.env.PORT || 5000, ()=>console.log('Server running ....'));
 
