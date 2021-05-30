0d1d6c5d0ad48c3862d409149f51dd23018b7336
Kunthy.Sen <kunthy.sen@PNCLB32-09.pnc.lan>
2021-05-30 23:52:31 +0700

    Done of Project 2

--- server.js
+++ server.js
@@ -1,9 +1,7 @@
 const express = require('express');
 const app = express();
 const fs = require('fs');
-let datausers=[]
-datausers =(JSON.parse(fs.readFileSync('dataUsers.json')));
-fs.writeFileSync('dataUsers.json',JSON.stringify(datausers));
+
 
 app.listen(process.env.PORT || 5000, ()=>console.log('Server running ....'));
 
@@ -11,42 +9,52 @@ app.use(express.json());
 app.use(express.urlencoded());
 app.use(express.static('public'));
 
-let users=[
-    {username:'thy',message:'welcome!!',color:'color-default'}
-];
+// read file from json for get data of all users
+let datausers =JSON.parse(fs.readFileSync('dataUsers.json'));
+// get data all of users and send it to client by using routing /datausers
+app.get('/datausers',(req,res)=> {
+    res.send(datausers)
+});
+//create new user that register 
+//get value from sign up and send it to client but it send only email
+app.post('/newuser',(req,res)=>{
+    let isEmail = false;
+    for(let user of datausers){
+        if(user.email===req.body.email){
+            isEmail=true;
+        }
+    }
+    if(!isEmail){
+        datausers.push(req.body)
+        fs.writeFileSync('dataUsers.json',JSON.stringify(datausers))
+    }
+    res.send(isEmail)
+    
+});
 
+// read file message from json and sent it to user
+app.get('/text',(req,res)=>{
+    let users = JSON.parse(fs.readFileSync('messages.json'));
+    res.send(users)
+});
 
-// let icons = [
-//     {id: 1, icon: '😓', sign: '):'},
-//     {id: 2, icon: '😄', sign: '(:'},
-//     {id: 3, icon: '😭', sign: 'cry'},
-//     {id: 4, icon: '🥰', sign: 'love'},
-//     {id: 5, icon: '😷', sign: 'sick'},
-//     {id: 6, icon: '😲', sign: 'wow'},
-//     {id: 7, icon: '😤', sign: 'bore'},
-//     {id: 8, icon: '🥱', sign: 'sleep'},
-//     {id: 9, icon: '😋', sign: 'haha'},
-//     {id: 10, icon: '🤬', sign: 'angry'},
-// ]
-
-// app.get('/emoji', (req, res) => res.send(icons));
-
-app.get('/datausers',(req,res)=>res.send(datausers));
-
-
-
-app.get('/text',(req,res)=>res.send(users));
-
+//get value of message from client send it to messages.json and write it then send back to client
 app.post('/text',(req,res)=>{
     let userName = req.body.username;
     let text = req.body.message;
     let color = req.body.color;
+    let time = req.body.time;
     let user={
         username:userName,
         message:text,
-        color:color
+        color:color,
+        time:time,
+        bold:req.body.bold,
+        italic:req.body.italic
     }
-    console.log(user)
-    users.push(user);
+    let users=[];
+    users = JSON.parse(fs.readFileSync('messages.json'));
+    users.push(user)
+    fs.writeFileSync('messages.json',JSON.stringify(users));
     res.send(users)
 })
